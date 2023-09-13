7b9a9b353dba93935e34a04f197276b51f0ae822
JKyEun <j56237@naver.com>
2023-09-13 12:29:58 +0900

    fix: 문법수정

--- pages/api/remove/[id].js
+++ pages/api/remove/[id].js
@@ -3,11 +3,10 @@ import { ObjectId } from 'mongodb';
 
 export default async function write(req, res) {
   if (req.method === 'DELETE') {
-    if (req.query === '') return res.status(500).send('body가 안왔슈');
     try {
       const client = await connectDB;
       const db = client.db('forum').collection('post');
-      const result = await db.deleteOne({ _id: new ObjectId(req.query.id) });
+      await db.deleteOne({ _id: new ObjectId(req.query.id) });
       return res.status(200).send('게시글 삭제 성공');
     } catch (err) {
       console.error(err);
